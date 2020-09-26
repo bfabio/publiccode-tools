a96d526f94341ef26170ea2d20048e89e7c54ddc
Fabio Bonelli <fb@fabiobonelli.it>
2020-09-26 16:12:40 +0200

    WIP

--- elasticsearch/entrypoint.sh
+++ elasticsearch/entrypoint.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-set -o xtrace
-set -o errexit
+TIMEOUT=60
 
-/usr/local/bin/docker-entrypoint.sh
+/usr/local/bin/docker-entrypoint.sh &
 
-elasticdump --input "$ELASTIC_DUMP_FILE" --output "$ELASTIC_SERVER"
+echo Waiting for Elasticsearch to start...
+
+count=0
+while true; do
+    RES="$(curl -sLf -m 2 -w "%{http_code}\n" "http://localhost:9200/" -o /dev/null)"
+    if [ "$RES" -ne "000" ]; then
+        break
+    fi
+
+    count=$((count + 1))
+    if [ $count -gt $TIMEOUT ]; then
+        echo "Can't connect to Elasticsearch after $TIMEOUT seconds"
+        exit 0
+    fi
+    sleep 1
+done
+
+echo Populating elasticsearch with development data...
+elasticdump --input "$ELASTIC_DUMP_FILE" --output http://localhost:9200
+
+echo "***************************************"
+echo "* Don't use this image in production  *"
+echo "***************************************"
 
 exec $@
